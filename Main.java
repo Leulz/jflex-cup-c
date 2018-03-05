95c282bf417e2e9f4b8e06a0802fa70f246d5b8d
Dênnis Dantas <ddspog@gmail.com>
2018-03-05 19:30:01 -0300

    feat(parser): Add writer to Assembly stage :construction:
    I've added a line to write the result as Assembly onto the same folder
    as the file in origin.

--- Main.java
+++ Main.java
@@ -1,19 +1,23 @@
 import java.io.*;
    
 public class Main {
-  static public void main(String argv[]) {    
-    /* Start the parser */
-    try {
-      parser p = new parser(new Lexer(new FileReader(argv[0])));
-      Object result;
-      if (argv.length > 1 && argv[1].equals("debug")) {
-        result = p.debug_parse().value;
-      } else {
-       result = p.parse().value;
-      }
-    } catch (Exception e) {
-      /* do cleanup here -- possibly rethrow e */
-      e.printStackTrace();
-    }
-  }
+	static public void main(String argv[]) {    
+		/* Start the parser */
+		try {
+    		parser p = new parser(new Lexer(new FileReader(argv[0])));
+			Object result;
+			if (argv.length > 1 && argv[1].equals("debug")) {
+				result = p.debug_parse().value;
+			} else {
+				result = p.parse().value;
+
+				FileWriter writer = new FileWriter(argv[0] + ".h");
+				writer.write(p.toAssemblyCode());
+				writer.close();
+			}
+		} catch (Exception e) {
+			/* do cleanup here -- possibly rethrow e */
+			e.printStackTrace();
+		}
+	}
 }
